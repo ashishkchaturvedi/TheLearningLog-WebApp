5a61f0ab8dc2df0dcfba58b2775f18e807189260
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2019-09-19 21:28:54 -0400

    Adding the first app<learning_logs>

---
+++ learning_logs/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Topic(models.Model):
+
+    """A topic that user is learning about"""
+    text = models.CharField(max_length = 200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+
+        """Reaturn a string representation of the model."""
+        return self.text
+
+
+class Entry(models.Model):
+
+    """Something specific learned about topic"""
+
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.text[:50] + "..."
