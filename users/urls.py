67b0c44fb407ff7a855ffb387cf7f76339f4bd47
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2019-09-26 23:52:34 -0400

    adding the login functionality

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+
+"""Define URL patterns for users"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+urlpatterns = [
+        # Login page
+        url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name="login"),
+                      
+]
